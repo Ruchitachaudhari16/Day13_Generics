bddd9e07f98670a1cfc92327ff6a07cfbcf18ab2
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-10 16:15:57 -0500

    [Ruchita] Add: Refactor of UC1 UC2 UC3

--- GenericsA13/GenericsA13/CheckMaximumNumber.cs
+++ GenericsA13/GenericsA13/CheckMaximumNumber.cs
@@ -8,9 +8,46 @@ namespace GenericsA13
 {
         internal class CheckMaximumNumber
         {
-        //UC3 given three string find maximum one
-         
-        //method for finding maximun string value
+        //Refactor 1- Refactor all the 3 toOne Generic Method and find the maximum
+        //UC1 check maximum number among three numbers 
+        //Checking for the integer values 
+        public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+        {
+            
+            if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+            {
+                return firstValue;
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+            {
+                return secondValue;
+            }
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+            {
+                return thirdValue;
+            }
+            throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+        }
+        //Checking for maximum float value 
+        //UC2 Maximum float value
+        public static float MaximumFloat(float first, float second, float third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            throw new Exception("first , second and third are same");
+        }
+
+        //UC3 method for finding maximun string value
         public static string MaximunStringNumber(string firstValue, string secondValue, string thirdValue)
         {
             if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
--- GenericsA13/GenericsA13/Program.cs
+++ GenericsA13/GenericsA13/Program.cs
@@ -5,7 +5,40 @@
         {
             static void Main(string[] args)
             {
-                Console.WriteLine("Find maximum  string among 3 string Values ");
+            //UC1 Maximum integer number
+            Console.WriteLine("Find maximum  number among 3 integer numbers ");
+            //Maximum number between three integer values 
+            //It will return first maximum value
+            int maxnumber = CheckMaximumNumber.MaximumIntegerNumber(95, 20, 30);
+            Console.WriteLine("Maximum integer number is :" + maxnumber);
+
+            //It will return Second maximum value 
+            int maxnumber1 = CheckMaximumNumber.MaximumIntegerNumber(20, 95, 30);
+            Console.WriteLine("Maximum integer number is:" + maxnumber1);
+
+            //It will return Third maximum value
+            int maxnumber2 = CheckMaximumNumber.MaximumIntegerNumber(30, 20, 95);
+            Console.WriteLine("Maximum integer number is:" + maxnumber2);
+            Console.WriteLine("*****************************************************************");
+
+            //UC2 Maximum float Value
+            //It will return First maximum float value
+            Console.WriteLine("Find maxmium number among 3 float numbers");
+            float maxno = CheckMaximumNumber.MaximumFloat(2.9f, 1.8f, 2.3f);
+            Console.WriteLine("Maximum float number is" + maxno);
+
+            //It will return Second maximum float value
+            float maxno1 = CheckMaximumNumber.MaximumFloat(1.8f, 2.9f, 2.3f);
+            Console.WriteLine("Maximum float number is:" + maxno1);
+            
+            //It will return Third maximum float value
+            float maxno2 = CheckMaximumNumber.MaximumFloat(1.8f, 2.3f, 2.9f);
+            Console.WriteLine("Maximum float number is:" + maxno2);
+            Console.WriteLine("*****************************************************************");
+
+            //UC3 Maximum String 
+
+            Console.WriteLine("Find maximum  string among 3 string Values ");
 
             //It will return maximum string value among 3 Strings.
             string maxstring = CheckMaximumNumber.MaximunStringNumber("Peach","Apple","Banana");
