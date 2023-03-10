1e07d760f3873d977fa123580606a22752b33485
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-03-10 15:16:15 -0500

    [Ruchita] Add: Maximum number from 3 numbers

---
+++ GenericsA13/GenericsA13/CheckMaximumNumber.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsA13
+{
+        internal class CheckMaximumNumber
+        {
+            //UC1 check maximum number among three numbers 
+            //Checking for the integer values 
+            public static int MaximumIntegerNumber(int firstValue, int secondValue, int thirdValue)
+            {
+                //string test = string.Empty;//""
+                if (firstValue.CompareTo(secondValue) > 0 && firstValue.CompareTo(thirdValue) > 0)
+                {
+                    return firstValue;
+                }
+                if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0)
+                {
+                    return secondValue;
+                }
+                if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(secondValue) > 0)
+                {
+                    return thirdValue;
+                }
+                throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+            }
+
+            //Checking for maximum float value 
+            public static float MaximumFloat(float first, float second, float third)
+            {
+                if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+                {
+                    return first;
+                }
+                if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+                {
+                    return second;
+                }
+                if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+                {
+                    return third;
+                }
+                throw new Exception("first , second and third are same");
+            }
+        }
+    }
+
+
---
+++ GenericsA13/GenericsA13/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace GenericsA13
+
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Find maximum  number among 3 numbers ");
+                //Maximum number between three integer values 
+                //It will return first maximum value
+                int maxnumber = CheckMaximumNumber.MaximumIntegerNumber(95, 20, 30);
+                Console.WriteLine("Maximum integer number is :" + maxnumber);
+
+                //It will return Second maximum value 
+                int maxnumber1 = CheckMaximumNumber.MaximumIntegerNumber(20, 95, 30);
+                Console.WriteLine("Maximum integer number is:" + maxnumber1);
+
+                //It will return Third maximum value
+                int maxnumber2 = CheckMaximumNumber.MaximumIntegerNumber(30, 20, 95);
+                Console.WriteLine("Maximum integer number is:" + maxnumber2);
+
+                //It will return First maximum float value
+                float maxno = CheckMaximumNumber.MaximumFloat(2.9f, 1.8f, 2.3f);
+                Console.WriteLine("Maximum float number is" + maxno);
+
+                //It will return Second maximum float value
+                float maxno1 = CheckMaximumNumber.MaximumFloat(1.8f, 2.9f, 2.3f);
+                Console.WriteLine("Maximum float number is:" + maxno1);
+
+                //It will return Third maximum float value
+                float maxno2 = CheckMaximumNumber.MaximumFloat(1.8f, 2.3f, 2.9f);
+                Console.WriteLine("Maximum float number is:" + maxno2);
+
+            }
+        }
+    }
